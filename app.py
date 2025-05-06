da59b35752b7626178310f55015fb1d36b7b3766
kazu217 <kazu20040127@acsl.ics.keio.ac.jp>
2025-05-06 21:41:59 +0900

    app.py 2143

--- app.py
+++ app.py
@@ -64,14 +64,16 @@ selected_game = st.selectbox("表示するゲームを選んでください", ga
 
 
 #ゲームごとの削除ボタン
-if st.button(f"⚠️『{selected_game}』のすべてのハンドを削除（元に戻せません）", type="primary"):
-    query = db.collection("hands").where("game", "==", selected_game).stream()
-    count = 0
-    for doc in query:
-        doc.reference.delete()
-        count += 1
-    st.success(f"『{selected_game}』のハンドを {count} 件 削除しました。")
-    st.rerun()
+if st.button(f"『{selected_game}』のすべてのハンドを削除（元に戻せません）", type="primary"):
+    confirm = st.checkbox("本当に削除しますか？")
+    if confirm:
+        query = db.collection("hands").where("game", "==", selected_game).stream()
+        count = 0
+        for doc in query:
+            doc.reference.delete()
+            count += 1
+        st.success(f"『{selected_game}』のハンドを {count} 件 削除しました。")
+        st.rerun()
 
 
 
